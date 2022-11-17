be1be5c095e185d56a0c1c624e282f8391e6324e
MENARD Jean Christophe (SNCF VOYAGEURS / ETAB REGIONAL COMMERCIAL NOUVELLE AQUITAINE / TER ESC GARE BDX DTL NA) <8207636u@commun.ad.sncf.fr>
2022-11-17 22:11:31 +0100

    commit jc

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
-
-
+//Extraire l'heure actuelle à l'aide de l'objet Date()
+let now = new Date();
 //Stocker l'heure , minute , seconde  dans des varaiables
-
+let heures = 
 
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
