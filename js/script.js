000ab1f13e933897ba1ee924fa947242a2582752
MENARD Jean Christophe (SNCF VOYAGEURS / ETAB REGIONAL COMMERCIAL NOUVELLE AQUITAINE / TER ESC GARE BDX DTL NA) <8207636u@commun.ad.sncf.fr>
2022-11-18 10:03:56 +0100

    commit code horloge 1.0

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
+clock();
+
+function clock() {
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+
+    const AIGUILLEHR = document.querySelector("#hour");
+    const AIGUILLEMIN = document.querySelector("#minute");
+    const AIGUILLESEC = document.querySelector("#second");
+
+
+    AIGUILLEHR.style.transform = `rotate(${hour}deg) `;
+
+    AIGUILLEMIN.style.transform = `rotate(${minute}deg)`;
+
+    AIGUILLESEC.style.transform = `rotate(${second}deg)`;
+}
+setInterval(clock ,1000);
 // Séléctionner les aiguilles de montre
-const AIGUILLEHR = document.querySelector("#hour");
-const AIGUILLEMIN = document.querySelector("#minute");
-const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuelle à l'aide de l'objet Date()
-let now = new Date();
+
 //Stocker l'heure , minute , seconde  dans des varaiables
-let heures = 
+
 
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
@@ -14,10 +34,9 @@ let heures =
 
 
 // Déplacer les aiguilles 
-function demarrerLaMontre() {
 
 
 
-}
+
+
 // Exercuter la fonction chaque second
-var interval = setInterval(demarrerLaMontre, 1000);
