c57e1d72ee58fca6853025350231b0c6ca377c52
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-18 11:01:06 +0100

    v0.21 fontion rotation

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ const AIGUILLEHR = document.querySelector("#hour");
 const AIGUILLEMIN = document.querySelector("#minute");
 const AIGUILLESEC = document.querySelector("#second");
 
-//Extraire l'heure actuelle à l'aide de l'objet Date()
 
+
+// Séléctionner les aiguilles de montre
+
+//Extraire l'heure actuelle à l'aide de l'objet Date()
 
 //Stocker l'heure , minute , seconde  dans des varaiables
 
@@ -15,10 +18,33 @@ const AIGUILLESEC = document.querySelector("#second");
 
 
 // Déplacer les aiguilles 
-function demarrerLaMontre() {
 
+function demarrerLaMontre() {
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6
+
+    
+    AIGUILLEHR.style.transform = `rotate(${hour}deg) `;
+
+    AIGUILLEMIN.style.transform = `rotate(${minute}deg)`;
+
+    AIGUILLESEC.style.transform = `rotate(${second}deg)`;
+}
+/* fonction qui fait bouger une aiguille definie, d'un certain nombre d'impulsion */
+function rotationAiguille (aiguille,impulsions) {
 
 
 }
+/* fonction qui initialise l'heure actuelle */
+function horlogeInit(){
+    
+}
+
+
 // Exercuter la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
