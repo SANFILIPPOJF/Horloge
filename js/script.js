957d95b0fb6895715e8d1ccf05f954bf84855725
MENARD Jean Christophe (SNCF VOYAGEURS / ETAB REGIONAL COMMERCIAL NOUVELLE AQUITAINE / TER ESC GARE BDX DTL NA) <8207636u@commun.ad.sncf.fr>
2022-11-18 14:11:08 +0100

    version 1.0

--- js/script.js
+++ js/script.js
@@ -1,38 +1,26 @@
-// Séléctionner les aiguilles de montre
-const AIGUILLEHR = document.querySelector("#hour");
-const AIGUILLEMIN = document.querySelector("#minute");
-const AIGUILLESEC = document.querySelector("#second");
+var interval = setInterval(demarrerLaMontre, 1000);
 
-//Extraire l'heure actuelle à l'aide de l'objet Date()
-const date = new Date();
-
-//Stocker l'heure , minute , seconde  dans des varaiables
-const hours = ((date.getHours() + 11) % 12 + 1);
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-console.log(`${hours}h ${minutes}min ${seconds}sec`);
-const nbSecondMinute = 60;
-const nbSecondHour = 3600;
-
-// Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque second selon un degré
-
-const angleSEC = 360 / 60;
-const angleMIN = angleSEC / 60;
-const angleHR = angleMIN / 12;
-console.log(`${angleSEC}°/sec ${angleMIN}°/minute ${angleHR}°/heure`);
-
-// Déplacer les aiguilles 
+demarrerLaMontre();
 
 function demarrerLaMontre() {
-    AIGUILLEHR.style.transform = `rotate(${impulsions*angleHR}deg) `;
+
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12 + 1);
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+    const AIGUILLEHR = document.querySelector("#hour");
+    const AIGUILLEMIN = document.querySelector("#minute");
+    const AIGUILLESEC = document.querySelector("#second");
+
+    AIGUILLEHR.style.transform = `rotate(${hour}deg)`;
+
+    AIGUILLEMIN.style.transform = `rotate(${minute}deg)`;
+
+    AIGUILLESEC.style.transform = `rotate(${second}deg)`;
+
+
 }
-
-/* fonction qui initialise l'heure actuelle */
-function horlogeInit(){
-    
-}
-
-
-// Exercuter la fonction chaque second
-var interval = setInterval(demarrerLaMontre, 1000);
+interval
