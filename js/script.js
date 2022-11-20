83c60d57c809e5a8103a1d4f500c2eac070a08b3
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-11-20 19:09:02 +0100

    v1.002 nettoyée

--- js/script.js
+++ js/script.js
@@ -41,32 +41,5 @@ function demarrerLaMontre() {
     AIGUILLESEC.style.transform = `rotate(${positionSeconde}deg)`;
     return;
 }
-// Exercuter la fonction chaque second
+// Exercuter la fonction chaque seconde
 var interval = setInterval(demarrerLaMontre, delaiCompteur);
-
-
-/* version du code "validée" vendredi
-
-var interval = setInterval(demarrerLaMontre, 1000);
-
-demarrerLaMontre();
-
-function demarrerLaMontre() {
-
-    const date = new Date();
-    const hours = ((date.getHours() + 11) % 12 + 1);
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    const hour = hours * 30;
-    const minute = minutes * 6;
-    const second = seconds * 6;
-    const AIGUILLEHR = document.querySelector("#hour");
-    const AIGUILLEMIN = document.querySelector("#minute");
-    const AIGUILLESEC = document.querySelector("#second");
-
-    AIGUILLEHR.style.transform = `rotate(${hour}deg)`;
-
-    AIGUILLEMIN.style.transform = `rotate(${minute}deg)`;
-
-    AIGUILLESEC.style.transform = `rotate(${second}deg)`;
-} */
